a805ebadefc99c2c93fcb9a9c152e27a461caf60
Winston McHetz <winstonmchetz@R2Hs-MBP.attlocal.net>
2022-02-28 17:50:50 -0500

    added constructs

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-const pokemon = null;
+// Create a class called pokemon with a constructer that takes in a name, type, and attack
+// Create a method for this class that console logs that the pokemon used its attack ex: "Pickachu, used Thunderbolt"
+// Instantiate 2 new pokemon then use the attack method for both of them.
+
+
+class pokemon {
+    constructor (properties) {
+        this.name = properties.name;
+        this.type = properties.type;
+        this.attack = properties.attack;
+    }
+    attack() {
+        return `${this.name} just used ${this.attack}`;
+    }
+
+}
+const pikachu = new pokemon({
+    name: 'pikachu',
+    type: 'electric',
+    attack: 'thunderbolt'
+})
+const charizard = new pokemon({
+    name: 'charizard',
+    type: 'fire',
+    attack: 'fireBlast'
+})
